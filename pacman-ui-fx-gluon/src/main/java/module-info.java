e832f6b46d174f7d68cc215d16e52420a3e69a86
Bruno Salmon <dev.salmonb@gmail.com>
2023-05-03 13:57:48 +0100

    Added Gluon module

---
+++ pacman-ui-fx-gluon/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module pacman.ui.fx.gluon {
+
+    // Direct dependencies modules
+    requires javafx.media;
+    requires pacman.ui.fx;
+    requires webfx.kit.openjfx;
+    requires webfx.platform.boot.java;
+    requires webfx.platform.console.java;
+    requires webfx.platform.os.gluon;
+    requires webfx.platform.resource.gluon;
+    requires webfx.platform.scheduler.java;
+    requires webfx.platform.shutdown.gluon;
+
+    // Meta Resource package
+    opens dev.webfx.platform.meta.exe;
+
+}
